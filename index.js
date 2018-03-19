b477cf1fd3c66712e334fa26a5ab0313e25dd1e8
Alan Liang <liangalann@gmail.com>
2018-03-19 21:13:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,10 +2,8 @@ var greeting = "Hello, everybody!";
 
 var specialGuest = "Neil deGrasse Tyson"
 
+var greetSpecialGuest = "Hello" + specialGuest + "!";
 
-var greetSpecialGuest = "" + specialGuest + "!";
-
-`Hello, ${specialGuest}!`
 
 var topic = "space";
 
